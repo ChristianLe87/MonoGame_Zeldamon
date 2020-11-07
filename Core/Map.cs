b093433b56100a1e7b2ef0c0abafa7493f07498f
Christian Lehnhoff <christianle87@icloud.com>
2020-11-07 19:04:49 +0100

    Add Map

---
+++ Core/Map.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shared
+{
+    internal class Map
+    {
+        Texture2D map_x;
+        Texture2D map_p;
+
+        private char[,] map1;
+
+        public Map(char[,] map1)
+        {
+            this.map1 = map1;
+            this.map_x = Tools.CreateColorTexture(Color.Brown);
+            this.map_p = Tools.CreateColorTexture(Color.Green);
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int row = 0; row < map1.GetLength(0); row++)
+            {
+                for (int elem = 0; elem < map1.GetLength(1); elem++)
+                {
+                    switch (map1[row, elem])
+                    {
+                        case 'x':
+                            spriteBatch.Draw(map_x, new Rectangle(x: elem * WK.Default.pixels_x, y: row * WK.Default.pixels_y, width: WK.Default.pixels_x, height: WK.Default.pixels_y), Color.White);
+                            break;
+                        case '.':
+                            spriteBatch.Draw(map_p, new Rectangle(x: elem * WK.Default.pixels_x, y: row * WK.Default.pixels_y, width: WK.Default.pixels_x, height: WK.Default.pixels_y), Color.White);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
--- Shared/Scenes/GameScene.cs
+++ Shared/Scenes/GameScene.cs
@@ -4,20 +4,24 @@ namespace Shared
 {
     public class GameScene : IScene
     {
+        Map map;
         Player player;
 
         public GameScene()
         {
+            map = new Map(WK.Map.Map1);
             player = new Player();
         }
 
         public void Update()
         {
+            map.Update();
             player.Update();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            map.Draw(spriteBatch);
             player.Draw(spriteBatch);
         }
     }
